ce1ca91ab389d3ff0206a07ff0a8c91bed671d1d
Aryan Jagarwal <ajbaggar@gmail.com>
2023-05-16 12:16:58 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Aryan Jagarwal on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

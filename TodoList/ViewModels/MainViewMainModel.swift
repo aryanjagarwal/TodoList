f41d3caec406cb14c7df30ed85753756a08a1d1e
Aryan Jagarwal <ajbaggar@gmail.com>
2023-06-11 18:52:31 +0530

    created users sign in

--- TodoList/ViewModels/MainViewMainModel.swift
+++ TodoList/ViewModels/MainViewMainModel.swift
@@ -5,4 +5,22 @@
 //  Created by Aryan Jagarwal on 16/05/23.
 //
 
+import FirebaseAuth
 import Foundation
+
+class MainViewViewModel: ObservableObject {
+    @Published var currentUserId: String = ""
+    private var handler: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] _, user in
+            DispatchQueue.main.async {
+                self?.currentUserId = user?.uid ?? ""
+            }
+        }
+    }
+    
+    public var isSignedIn: Bool {
+        Auth.auth().currentUser != nil
+    }
+}
--- TodoList/Views/MainView.swift
+++ TodoList/Views/MainView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct MainView: View {
+    @StateObject var viewModel = MainViewViewModel()
+    
     var body: some View {
-        
-        LoginView()
-        
+        if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
+            // signed in state
+            ToDoListView()
+        } else {
+            LoginView()
+        }
     }
 }
 
